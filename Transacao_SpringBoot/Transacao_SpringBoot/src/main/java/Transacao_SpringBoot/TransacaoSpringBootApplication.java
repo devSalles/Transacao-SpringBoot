359e808b317f448dcd101d95cd5c6f9137889755
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2025-09-29 15:52:36 -0300

    Subindo projeto

---
+++ Transacao_SpringBoot/Transacao_SpringBoot/src/main/java/Transacao_SpringBoot/TransacaoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package Transacao_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoSpringBootApplication.class, args);
+	}
+
+}
---
+++ Transacao_SpringBoot/Transacao_SpringBoot/src/test/java/Transacao_SpringBoot/TransacaoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package Transacao_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
